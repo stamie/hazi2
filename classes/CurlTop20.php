c586b0cf1a8be3cf5895a68b892fdcedea86ee64
stamie <estampel@gmail.com>
2022-01-22 18:03:20 +0100

    make curltests and repair the bugs

--- classes/CurlTop20.php
+++ classes/CurlTop20.php
@@ -1,10 +1,12 @@
 <?php
 namespace app\classes;
 class CurlTop20 {
-    const SITE_URL = 'https://www.imdb.com/chart/top/';
+    const SITE_URL_TOP = 'https://www.imdb.com/chart/top/';
+    const SITE_URL     = 'https://www.imdb.com/';
+
     public static function loadTop250page(){
         $ch = curl_init();
-        curl_setopt( $ch, CURLOPT_URL, self::SITE_URL );
+        curl_setopt( $ch, CURLOPT_URL, self::SITE_URL_TOP );
         curl_setopt( $ch, CURLOPT_POST, false ); 
         curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
 
--- controllers/TestsController.php
+++ controllers/TestsController.php
@@ -6,10 +6,16 @@ use app\classes\XmlUse;
 
 class TestsController extends \yii\web\Controller
 {
-    public function actionCurltest()
+    const FILM_URL = 'title/tt0111161/?pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=9703a62d-b88a-4e30-ae12-90fcafafa3fc&pf_rd_r=8YBFN9BAGNFFNPSKX3SG&pf_rd_s=center-1&pf_rd_t=15506&pf_rd_i=top&ref_=chttp_tt_1';
+    public function actionCurltesttop250()
     {
         $exec = CurlTop20::loadTop250page();
-        return $this->render('index', ['exec' => $exec]);
+        return $this->render('curltesttop250', ['exec' => $exec]);
+    }
+    public function actionCurltestfilmpage(string $url = self::FILM_URL)
+    {
+        $exec = CurlTop20::loadMovie($url);
+        return $this->render('curltestfilmpage', ['exec' => $exec]);
     }
     
     
