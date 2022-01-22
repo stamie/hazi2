9b31481a31341be59440d7e0a21078cf0148c41f
stamie <estampel@gmail.com>
2022-01-22 17:44:30 +0100

    test controller and view

--- controllers/ToplistController.php
+++ controllers/ToplistController.php
@@ -7,7 +7,7 @@ class ToplistController extends \yii\web\Controller
 {
     public function actionIndex()
     {
-        $exec = CurlTop20::loadPage();
+        $exec = CurlTop20::loadTop250page();
         return $this->render('index', ['exec' => $exec]);
     }
     
